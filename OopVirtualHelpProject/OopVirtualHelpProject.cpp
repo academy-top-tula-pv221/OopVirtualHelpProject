a2a5c61a4bf01be3308fbbfe3bee02b691a18a02
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-18 20:10:22 +0300

    Добавьте файлы проекта.

---
+++ OopVirtualHelpProject/OopVirtualHelpProject.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+class A
+{
+public:
+    virtual void M()
+    {
+        std::cout << "A\n";
+    }
+};
+
+class B : public A
+{
+public:
+    virtual void M()
+    {
+        std::cout << "B\n";
+    }
+};
+
+class C : public A
+{
+public:
+    virtual void M()
+    {
+        std::cout << "C\n";
+    }
+};
+
+class D : public B
+{
+public:
+    
+};
+
+class E : public D
+{
+public:
+    void EEEE()
+    {
+
+    }
+};
+
+
+int main()
+{
+    A* aptr = new A();
+    aptr->M();
+
+    aptr = new B();
+    aptr->M();
+
+    aptr = new D();
+    aptr->M();
+
+    aptr = new E();
+    aptr->M();
+    
+    E* eptr = new E();
+    eptr->EEEE();
+}
